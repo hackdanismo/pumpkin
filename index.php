8e2bbbf6f2cd073c97834b641998a6a784911173
hackdanismo <office@justtoconfirm.co.uk>
2024-11-07 16:18:14 +0000

    feat: setup the pmkn_config.php file to store database and configuration credentials

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
-    // Database connection variables
-    $hostname = "localhost";
-    $username = "username";
-    $password = "password";
-    $database = "pumpkin_test_db";
+    // Include the configuration file
+    require_once "pmkn_config.php";
 
     try {
-        // Create a PDO instance with MySQL DSN (Data Source Name)
-        $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+        // Create a PDO instance with MySQL DSN (Data Source Name) using values from the pmkn_config.php file
+        $conn = new PDO("mysql:host=" .DB_HOST .";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
         // Set the PDO error mode to exception for better error handling
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Database Connected";
---
+++ pmkn_config.php
@@ -0,0 +1,5 @@
+<?php
+    define("DB_HOST", "localhost");
+    define("DB_USER", "username");
+    define("DB_PASSWORD", "password");
+    define("DB_NAME", "pumpkin_test_db");
