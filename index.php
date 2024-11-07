250069137b2c0b0f9721a57b6e69f0af9230ae44
hackdanismo <office@justtoconfirm.co.uk>
2024-11-07 16:10:47 +0000

    feat: setup connection to a MySQL database using PHP PDO

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
-    echo "Welcome to Pumpkin";
+    // Database connection variables
+    $hostname = "localhost";
+    $username = "username";
+    $password = "password";
+    $database = "pumpkin_test_db";
+
+    try {
+        // Create a PDO instance with MySQL DSN (Data Source Name)
+        $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+        // Set the PDO error mode to exception for better error handling
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Database Connected";
+    } catch(PDOException $e) {
+        // Display the exact error during development, better to remove and log this in production
+        echo "Connection failed: " . $e->getMessage();
+    }
