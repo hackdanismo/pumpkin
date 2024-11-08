14f827121f6c41fb4d2cba838c50b100e51c9158
hackdanismo <office@justtoconfirm.co.uk>
2024-11-08 18:17:32 +0000

    feat: add fields for setting up the database credentials on the install.php page

--- admin/install.php
+++ admin/install.php
@@ -7,6 +7,8 @@ error_reporting(E_ALL);
 // Include the database class includes file to access the database
 require_once __DIR__ . "/../includes/pmkn_database.php";
 
+// This is the initial page that allows users to setup the database credentials and create the admin table
+
 // Check if the form has been submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the submitted database name, or use the default value is no name is provided
@@ -29,8 +31,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <h1>Pumpkin Install</h1>
 
     <form method="POST" action="">
+        <label for="db_host">Hostname:</label>
+        <input type="text" id="db_host" name="db_host" placeholder="localhost"><br>
+        <label for="db_user">Username:</label>
+        <input type="text" id="db_user" name="db_user" placeholder="username"><br>
+        <label for="db_password">Password:</label>
+        <input type="password" id="db_password" name="db_password" placeholder="password"><br>
         <label for="db_name">Database Name (leave empty for the default):</label>
-        <input type="text" id="db_name" name="db_name" placeholder="pmkn_db">
+        <input type="text" id="db_name" name="db_name" placeholder="pmkn_db"><br>
         <button type="submit">Install</button>
     </form>
 </body>
